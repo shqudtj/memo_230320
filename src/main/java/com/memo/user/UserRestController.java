b6a3426e5996b075637c0941b2546a739c9c17f1
test <test@test.com>
2023-07-17 18:45:28 +0900

    memo 로그인 화면까지 됐음

--- src/main/java/com/memo/user/UserRestController.java
+++ src/main/java/com/memo/user/UserRestController.java
@@ -3,14 +3,28 @@ package com.memo.user;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.memo.common.EncryptUtils;
+import com.memo.user.bo.UserBO;
+import com.memo.user.entity.UserEntity;
 
 @RequestMapping("/user")
 @RestController
 public class UserRestController {
 
+	@Autowired
+	private UserBO userBO;
+	
+	/**
+	 * 중복확인 API
+	 * @param loginId
+	 * @return
+	 */
 	@RequestMapping("/is_duplicated_id")
 	public Map<String, Object> isDuplicatedId(
 				@RequestParam("loginId") String loginId
@@ -19,10 +33,66 @@ public class UserRestController {
 		Map<String, Object> result = new HashMap<>();
 		
 		// select
-		result.put("code", 1);
-		result.put("isDuplicatedId", true);
+		UserEntity userEntity = userBO.getUserEntityByLoginId(loginId);
+		
+		if (userEntity != null) {
+			result.put("code", 1);
+			result.put("isDuplicatedId", true);
+		} else {
+			result.put("code", 1);
+			result.put("isDuplicatedId", false);
+		}
+		
+		return result;
+	}
+	
+	
+	/**
+	 * 회원가입 API
+	 * @param loginId
+	 * @param password
+	 * @param name
+	 * @param email
+	 * @return
+	 */
+	@PostMapping("/sign_up")
+	public Map<String, Object> signUp(
+			@RequestParam("loginId") String loginId,
+			@RequestParam("password") String password,
+			@RequestParam("name") String name,
+			@RequestParam("email") String email
+			) {
+		
+		// 비밀번호 해싱 - md5 알고리즘 ( 취약한 알고리즘이지만 쉬워서 지금만 사용 다른 거 할 시에 딴거 해보기)
+		// aaaa => 74b8733745420d4d33f80c4663dc5e5 라면
+		// aaaa => 74b8733745420d4d33f80c4663dc5e5 으로 같은 값입력시 같은 암호가 나와서 보안에 취약함
+		String hashedPassword = EncryptUtils.md5(password);
+		
+		// db insert
+		Integer userId = userBO.addUser(loginId, hashedPassword, name, email);
+		
+		Map<String, Object> result = new HashMap<>();
+		if (userId != null) {
+			result.put("code" , 1);
+			result.put("result" , "성공");
+		} else {
+			result.put("code" , 500);
+			result.put("errorMessage" , "회원가입 하는데 실패했습니다.");
+		}
 		
 		return result;
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
